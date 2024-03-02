65fe7ccb78eb32d5213ceabaad7be857889b83bd
ataberkkemer <ataberkkemer@gmail.com>
2024-03-02 21:07:33 +0300

    save system update

---
+++ Source/CurseOfTheSeven/Private/Save/SaveSystem.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Save/SaveSystem.h"
+
+USaveSystem::USaveSystem()
+{
+	LevelName = TEXT("TestSaveSlot");
+}
